509519d26ae88b1eff26d6f80ecac1b2708ef7a7
Silvia Popescu <amazingaivlis@gmail.com>
2020-11-24 23:16:17 -0800

    Created cards for five day cast, formatted data, formatted dates.

--- script.js
+++ script.js
@@ -1,70 +1,102 @@
-// To trigger the AJAX call
-$("#search-city").on("click", function(event) {
-    event.preventDefault();
+$(document).ready(function() {
 
-    //Gets the text from city input box
-    var city = $("#city-input").val();
+    // To trigger the AJAX call
+    $("#search-city").on("click", function(event) {
+        event.preventDefault();
 
-    // Creating variable for API Key
-    var APIKey = "e0c3e9cab48d34d878451828cf2fdc20";
+        //Gets the text from city input box
+        var city = $("#city-input").val();
 
-    // Building the URL to query the database
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+        // Creating variable for API Key
+        var APIKey = "e0c3e9cab48d34d878451828cf2fdc20";
 
-    // AJAX call for current conditions
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        //console log to see what the information stored in object
-        console.log(queryURL);
-        console.log(response);
+        // Building the URL to query the database
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
-        // Creates an element to hold city name
-        var cityDisplay = $("<h1>").text(city);
+        // AJAX call for current conditions
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            //console log to see what the information stored in object
+            console.log(queryURL);
+            console.log(response);
 
-        // Retrieves temperature in Kelvin
-        var tempK = response.main.temp;
-        // Converts temperature to Farhenheit
-        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-        // Creates an element to hold tempF, rounding to one decimal place
-        var temperature = $("<p>").text("Temperature: " + tempF.toFixed(1));
+            // Creates an element to hold city name
+            var cityDisplay = $("<h1>").text(city);
 
-        // Creates variable for humidity and element to store it
-        var humidity = $("<p>").text("Humidity: " + response.main.humidity);
+            // To show icon for today's weather
+            var todayIconURL = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+            var todayIcon = $("<img>").attr("src", todayIconURL).css("width", "64px").css("height", "64px");
+        
+            // Creates variable for today's date and an element to store it
+            var today = $("<h4>").text(moment().format('l'));
 
-        // Creates variable for wind speed and element to store it 
-        var windSpeed = $("<p>").text("Wind Speed: " + response.wind.speed);
+            // Retrieves temperature in Kelvin and converts to Fahrenheit
+            var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+            // Creates an element to hold tempF, rounding to one decimal place
+            var temperature = $("<p>").text("Temperature: " + tempF.toFixed(1));
 
-        //Creates variable for UV index and element to store it
-        //var uvIndex = $("<p>").text("UV Index: " + response.);
+            // Creates variable for humidity and element to store it
+            var humidity = $("<p>").text("Humidity: " + response.main.humidity);
 
-        // Appends current data to card created in HTML
-        $("#current-city-data").append(cityDisplay, temperature, humidity, windSpeed);
+            // Creates variable for wind speed and element to store it 
+            var windSpeed = $("<p>").text("Wind Speed: " + response.wind.speed);
+
+            //Creates variable for UV index and element to store it
+            var uvURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + response.coord.lat + "&lon=" + response.coord.lat + "&appid=" + APIKey;
+            var uvIndex = $("<p>").text("UV Index: " + uvURL);
+            console.log(uvIndex);
+
+            // Appends current data to card created in HTML
+            $("#current-city-data").append(cityDisplay, todayIcon, today, temperature, humidity, windSpeed, uvIndex);
+        });
+
+        // Create variable for five day forecast URL
+        var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
+
+        // AJAX call for five day forecase
+        $.ajax({
+            url: fiveDayURL,
+            method: "GET"
+        }).then(function(response) {
+            //console log to see what the information stored in object
+            console.log(fiveDayURL);
+            console.log(response);
+            
+            // for loop to iterate over the next 5 days
+            for (let i = 1; i <= 41; i+=8) {
+
+                // Variable to simplify tranversing the DOM
+                var loopResponse = response.list[i];
+            
+                // Separates and formats date, creates variable and element to store it in
+                var getDate = loopResponse.dt_txt.split(" ");
+                var splitDate = getDate[0].split("-");
+                var formatDate = $("<h5>").text(splitDate[1] + "/" + splitDate[2] + "/" + splitDate[0]);
+                
+                // Creates variable for icon and creates element to store it
+                var iconURL = "http://openweathermap.org/img/w/" + loopResponse.weather[0].icon + ".png";
+                var displayIcon = $("<img>").attr("src", iconURL);
+
+                // Creates temperature variable, converts from Kelvin to Fahrhenheit and element to store it in
+                var fiveDayTemp = $("<p>").text("Temp: " + ((loopResponse.main.temp - 273.15) * 1.80 + 32).toFixed(1));
+
+                //Create humidity variable and element to store it in
+                var fiveDayHumidity = $("<p>").text("Humidity: " + loopResponse.main.humidity);
+
+
+                //Create a card for each iteration
+                cardCreate = $("<div>").addClass("card text-white bg-primary mb-3").attr("id", "one-card");
+                // Append to the deck
+                $("#card-deck").append(cardCreate);
+                // Now we make the body of the card, that will hold our data. Adding id and class
+                cardBodyCreate = $("<div>").addClass("card-body").attr("id", "daily-forecast");
+                // here we target the first card div by the id we gave it of "one-card", and append the cardbody to it
+                $("#one-card").append(cardBodyCreate);
+                // Then we target the card body by the id we gave it of daily-forecast, and append all of our data to it
+                $("#daily-forecast").append(formatDate, displayIcon, fiveDayTemp, fiveDayHumidity);
+            }
+        });
     });
-
-    // Create variable for five day forecast URL
-    var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
-
-    // AJAX call for five day forecase
-    $.ajax({
-        url: fiveDayURL,
-        method: "GET"
-    }).then(function(fiveDayResponse) {
-        //console log to see what the information stored in object
-        console.log(fiveDayURL);
-        console.log(fiveDayResponse);
-
-        // for loop to iterate over the next 5 days
-        for (var i = 1; i <= 5; i++) {
-            // Creates temperature variable and element to store it in
-            var fiveDayTemp = $("<p>").text("Temp: " + ((fiveDayResponse.list[1].main.temp - 273.15) * 1.80 + 32).toFixed(1));
-
-            //Create humidity variable and element to store it in
-            var fiveDayHumidity = $("<p>").text("Humidity: " + fiveDayResponse.list[1].main.humidity);
-
-            // Appends data to card created in HTML
-            $("#five-day").append(fiveDayTemp, fiveDayHumidity);
-        }
-    })
 });
